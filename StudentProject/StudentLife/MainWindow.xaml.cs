b96f385ce826357b0c79f7b259fd1e9627fb151c
dt <t6d2@yahoo.it>
2018-08-02 21:23:27 +0200

    risolti altri bug

--- StudentProject/StudentLife/MainWindow.xaml.cs
+++ StudentProject/StudentLife/MainWindow.xaml.cs
@@ -20,69 +20,69 @@ namespace StudentLife
 {
     public partial class MainWindow : Window
     {
-        public static DbConnection dbc;
+        public static DbConnection dbConnection;
 
         public MainWindow()
         {
             InitializeComponent();
 
             InitializeData();
-            this.contentMainWindow.Content = new DataDisplay(dbc);
+            this.contentMainWindow.Content = new DataDisplay(dbConnection);
             AllActivities_MenuItem.Visibility = Visibility.Collapsed;
             ActivitiesBySubject_MenuItem.Visibility = Visibility.Visible;
         }
 
         private void InitializeData()
         {
-            dbc = new DbConnection();
+            dbConnection = new DbConnection();
         }
 
         private void Subjects_Click(object sender, RoutedEventArgs e)
         {
-            MenuItemOperations(sender, dbc => new DataSubject(dbc));
+            MenuItemOperations(sender, dbConnection => new DataSubject(dbConnection));
         }
         private void ClassRoomTasks_Click(object sender, RoutedEventArgs e)
         {
-            MenuItemOperations(sender, dbc => new DataClassRoomTask(dbc));
+            MenuItemOperations(sender, dbConnection => new DataClassRoomTask(dbConnection));
         }
 
         private void Homework_Click(object sender, RoutedEventArgs e)
         {
-            MenuItemOperations(sender, dbc => new DataHomework(dbc));
+            MenuItemOperations(sender, dbConnection => new DataHomework(dbConnection));
         }
 
         private void ClassTask_Click(object sender, RoutedEventArgs e)
         {
-            MenuItemOperations(sender, dbc => new DataClassRoomTaskType(dbc));
+            MenuItemOperations(sender, dbConnection => new DataClassRoomTaskType(dbConnection));
         }
 
         private void MenuItemOperations(object sender, Func<DbConnection, UserControl> ucCreator)
         {
             var clickedMenuItem = sender as MenuItem;
             var textHeader = clickedMenuItem.Header.ToString();
-            contentMainWindow.Content = ucCreator(dbc);
+            contentMainWindow.Content = ucCreator(dbConnection);
             AllActivities_MenuItem.Visibility = Visibility.Visible;
             ActivitiesBySubject_MenuItem.Visibility = Visibility.Visible;
         }
 
         private void Activities_Click(object sender, RoutedEventArgs e)
         {
-            this.contentMainWindow.Content = new DataDisplay(dbc);
+            this.contentMainWindow.Content = new DataDisplay(dbConnection);
             AllActivities_MenuItem.Visibility = Visibility.Collapsed;
             ActivitiesBySubject_MenuItem.Visibility = Visibility.Visible;
         }
 
         private void MaterialActivities_Click(object sender, RoutedEventArgs e)
         {
-            this.contentMainWindow.Content = new DataDisplayByMaterial(dbc);
+            this.contentMainWindow.Content = new DataDisplayByMaterial(dbConnection);
             AllActivities_MenuItem.Visibility = Visibility.Visible;
             ActivitiesBySubject_MenuItem.Visibility = Visibility.Collapsed;
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if (dbc.SqlConn != null)
-                dbc.SqlConn.Close();
+            if (dbConnection.SqlConn != null)
+                dbConnection.SqlConn.Close();
         }
     }
 }
